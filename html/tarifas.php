674320a53950900112988f3cb9ad073d7bec4dde
Emmanuel <rereocross@outlook.com>
2019-11-25 09:40:12 -0600

    conexiones

--- html/tarifas.php
+++ html/tarifas.php
@@ -11,8 +11,8 @@
 
   <!-- Firebase -->
 
-  <script src="/__/firebase/6.2.0/firebase-auth.js"></script>
-  <script src="/__/firebase/6.2.0/firebase-firestore.js"></script>
+ <script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js"></script>
+ <script src="https://www.gstatic.com/firebasejs/6.6.2/firebase-firestore.js"></script>
 
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -150,5 +150,15 @@
 <script src="../build/js/app.js">
   
 </script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.5.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.5.0/firebase-analytics.js"></script>
+
 </body>
 </html>
